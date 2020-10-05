6884853284b3423149a6a78203ae91fe05e16a93
Deekshant Wadhwa <deekshantwadhwa2000@gmail.com>
2020-10-05 20:06:57 +0530

    Gunicorn Configured Successfully : Final Cleaning : Removing Debuging code

--- main.py
+++ main.py
@@ -5,21 +5,18 @@ from crud import main as crud
 
 app = FastAPI()
 
-# Image router
 app.include_router(
     image.app,
     prefix="/image",
     tags=["Image Functions"],
 )
 
-# text router
 app.include_router(
     text.router,
     prefix="/text",
     tags=["Text Functions"],
 )
 
-# crud router
 app.include_router(
     crud.router,
     prefix="/crud",

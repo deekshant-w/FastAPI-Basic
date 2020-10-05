a7d0c9e9f020470b82cd5e8d51cdcea1b7cd7887
Deekshant Wadhwa <deekshantwadhwa2000@gmail.com>
2020-10-05 16:35:08 +0530

    Added comments in main

--- main.py
+++ main.py
@@ -5,18 +5,21 @@ from crud import main as crud
 
 app = FastAPI()
 
+# image router
 app.include_router(
     image.app,
     prefix="/image",
     tags=["Image Functions"],
 )
 
+# text router
 app.include_router(
     text.router,
     prefix="/text",
     tags=["Text Functions"],
 )
 
+# crud router
 app.include_router(
     crud.router,
     prefix="/crud",
